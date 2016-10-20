503acd2b636c3c8d2f897a4ef9424a4765e64020
lutzer <lutz@Lutzs-MacBook-Pro.local>
2016-10-21 01:30:37 +0200

    changed key grabber

--- src/keyboard/keygrabber/main.py
+++ src/keyboard/keygrabber/main.py
@@ -1,78 +1,92 @@
 # -*- coding: utf-8 -*-
 # @Author: Lutz Reiter, Design Research Lab, Universität der Künste Berlin
-# @Date:   2016-10-19 10:45:09
+# @Date:   2016-10-20 23:45:27
 # @Last Modified by:   lutzer
-# @Last Modified time: 2016-10-20 16:21:35
+# @Last Modified time: 2016-10-21 01:28:29
 
+import curses
 import logging
-import pyglet
-import time
-import sys
 
 from comm.keyboardSocketSender import KeyboardSocketSender
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
-# params
 SERVER_ADDRESS = 'localhost'
 SERVER_PORT = 8091
 
-# global vars
+#logging.basicConfig(level=logging.DEBUG)
+#logger = logging.getLogger(__name__)
+
+running = True
+stdscr = None
 socket = None
-window = None
-
-class Window(pyglet.window.Window):
-
-	def __init__(self):
-		pyglet.window.Window.__init__(self);
-		pyglet.app.run()
-
-	def stop(self):
-		pyglet.app.exit()
-
-	def on_key_press(self, symbol, modifiers):
-		if symbol == pyglet.window.key.ESCAPE:
-			self.stop()
-
-	def on_text(self, text):
-		onKeypress(text)
-
-	def on_text_motion(self, motion):
-		onKeypress(motion,motion=True)
 
 def init():
-	global socket, window
-	logger.info("starting service")
+	global stdscr, socket
+
+	logging.info('started keygrabber')
 
 	# start socket
 	socket = KeyboardSocketSender(SERVER_ADDRESS,SERVER_PORT)
 	socket.start()
 
-	# start window for keylistener
-	window = Window()
+	#init the curses screen
+	stdscr = curses.initscr()
+	# turn of echo
+	curses.noecho()
+	# do not require enter 
+	curses.cbreak()
+	# enable arrow keys
+	stdscr.keypad(1) 
 
 def stop():
-	global socket, window
-	logger.info("stopping service")
-	window.stop()
+	global stdscr, socket
+
+	logging.info('stopped keygrabber')
+	# listening and revert console
+	curses.nocbreak()
+	stdscr.keypad(0) 
+	curses.echo()
+	curses.endwin()
+
 	socket.stop()
-	sys.exit(0)
 
 def loop():
-	# stop program when reaching loop
-	stop()
+	global stdscr, running, socket
+
+	c = stdscr.getch()
+	# output key press
+
+	if c == 27: # escape key, quit program
+		running = False
+	elif c == curses.KEY_LEFT:
+		onKeypress(65361,motion=True)
+	elif c == curses.KEY_RIGHT:
+		onKeypress(65363,motion=True)
+	elif c == curses.KEY_UP:
+		onKeypress(65362,motion=True)
+	elif c == curses.KEY_DOWN:
+		onKeypress(65364,motion=True)
+	elif c == 127 or c == 330:
+		onKeypress(65288,motion=True)
+	else:
+		onKeypress(c)
 
 def onKeypress(key,motion=False):
 	global socket
+	
+	printKey(key)
+
 	if motion:
 		socket.sendTextMotion(key)
 	else:	
 		socket.sendText(key)
 
-init()
+def printKey(string):
+	stdscr.addstr(0, 0, '                           ')
+	stdscr.addstr(0, 0, 'key pressed: ' + str(string))
+
 try:
-	while True:
+	init()
+	while running:
 		loop()
 except KeyboardInterrupt:
    print("# program loop interrupted")
